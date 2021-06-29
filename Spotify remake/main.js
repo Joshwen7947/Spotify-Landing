ca52befc9da0eaca0c7132bb6afa22dad397d4e6
Josh Wenner <68701564+Joshwen7947@users.noreply.github.com>
2021-06-29 14:39:02 +0700

    Add files via upload

---
+++ Spotify remake/main.js
@@ -0,0 +1,21 @@
+// alert(`hello`);
+`use strict`;
+// function isPrime(number) {
+// 	if (number === 2) {
+// 		return true;
+// 	} else if (number > 1) {
+// 		for (let i = 2; i < number; i++) {
+// 			if (number % i !== 0) {
+// 				return true;
+// 			} else if (number === i * i) {
+// 				return false;
+// 			} else {
+// 				return false;
+// 			}
+// 		}
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// console.log(isPrime(109));
